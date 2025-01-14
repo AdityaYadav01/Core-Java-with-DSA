7ad2a6b74aba110a8d64bc7ec53292066d769d33
Aditya Yadav <adityayadav44556@gmail.com>
2025-01-14 19:32:18 +0530

    Pattern Problem-2

--- Loops/src/Ques18.java
+++ Loops/src/Ques18.java
@@ -1,9 +1,9 @@
 /* Display this pattern
-*       1 1 1 1 1
-        2 2 2 2 2
-        3 3 3 3 3
-        4 4 4 4 4
-        5 5 5 5 5
+*       2 3 4 5
+        3 4 5 6
+        4 5 6 7
+        5 6 7 8
+        6 7 8 9
 * */
 
 
@@ -11,7 +11,7 @@ public class Ques18 {
     public static void main(String[] args) {
         for (int i = 1; i <=5 ; i++) {
             for (int j = 1; j < 5; j++) {
-                System.out.print(i + " ");
+                System.out.print((i+j) + " ");
             }
             System.out.println();
 
